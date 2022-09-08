806eebff539c17183c9ed0a09109b964e15bd487
lalarustamli <rustamlilala@gmail.com>
2022-09-08 15:20:05 +0300

    Add common errors and messages

---
+++ app/errors.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Errors
+  class ValidationError < StandardError
+    def message
+      puts('The command is used incorrectly. Type `help` for instructions')
+    end
+  end
+
+  class NoWarehouseError < StandardError
+    def message
+      puts('Please create a warehouse. Type `help` for instructions')
+    end
+  end
+
+  class NoCrateError < StandardError
+    def message
+      puts('Crate with given code does not exist')
+    end
+  end
+
+  class ArgumentError < StandardError
+    def message
+      puts('Coordinates should we higher than 0')
+    end
+  end
+
+  class CapacityError < StandardError
+    def message
+      puts('Not enough space in warehouse')
+    end
+  end
+end
